a512803ff33573db7c726f967ee8dd95497e8836
David Morales <daamoralesm@correo.udistrital.edu.co>
2019-06-12 16:32:37 -0500

    se corrige migracion
    se corrige migracion de relaciones de esquema plan_cuentas

--- database/migrations/20190526_121612_apropiacion_table.go
+++ database/migrations/20190526_121612_apropiacion_table.go
@@ -1,4 +1,4 @@
-package migrations
+package main
 
 import (
 	"github.com/astaxie/beego/migration"
@@ -23,7 +23,7 @@ func (m *ApropiacionTable_20190526_121612) Up() {
 	m.SQL("ALTER TABLE apropiacion ADD CONSTRAINT fk_apropiacion_rubro FOREIGN KEY (rubro_id) REFERENCES rubro(id) MATCH FULL;")
 	m.SQL("ALTER TABLE rama ADD CONSTRAINT fk_rubro_hijo FOREIGN KEY (rubro_hijo) REFERENCES rubro(id) MATCH FULL;")
 	m.SQL("ALTER TABLE rama ADD CONSTRAINT fk_rubro_padre FOREIGN KEY (rubro_padre) REFERENCES rubro(id) MATCH FULL;")
-	m.SQL("ALTER TABLE apropiacion ADD CONSTRAINT fk_apropiacion_estado_apropiacion FOREIGN KEY (estado) REFERENCES estado_apropiacion(id) ON UPDATE CASCADE ON DELETE RESTRICT;")
+	m.SQL("ALTER TABLE apropiacion ADD CONSTRAINT fk_apropiacion_estado_apropiacion FOREIGN KEY (estado_apropiacion_id) REFERENCES estado_apropiacion(id) ON UPDATE CASCADE ON DELETE RESTRICT;")
 }
 
 // Reverse the migrations
